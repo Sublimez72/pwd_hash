abeceeeb3def68c5d1b946f33cefdf3d0edcdd0a
Sublimez72 <vaggge@hotmail.com>
2023-03-23 13:24:27 +0100

    Changed log level

--- server.py
+++ server.py
@@ -20,6 +20,7 @@ except:
     log.error('Failed to import required third-party module "flask"')
     exit(1)
 
+os.environ["LOG_LEVEL"] = "DEBUG"
 # Configure application log level
 if 'LOG_LEVEL' in os.environ and os.environ['LOG_LEVEL'] == 'INFO':
     log_level = log.INFO
